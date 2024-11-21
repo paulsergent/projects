1bbc741ed0264a58a7b21085b08e5d227e394c8b
paulsergent <paulsergent.pro@gmail.com>
2024-11-21 16:01:59 +0100

    feat: add shop and images in it

--- src/app/shop/page.jsx
+++ src/app/shop/page.jsx
@@ -1,17 +1,18 @@
 import { products } from '../components/data.js';
 import king from "../components/product.module.css";
+import Image from 'next/image';
 
 export default function ShopPage() {
     const productCat = products.map(product => (
-        <section key=(product.id)>
-            <h2>{product.name}</h2>
-            <p>{product.description}</p>
-            <p>${product.price}</p>
+        <section key={product.id} className="flex flex-col items-center hover:shadow-2xl">
+            <Image src = {product.imageUrl} alt={product.category} width={100} height={80}/>
+            <p>ID: {product.id}</p>
+            <h2>Name: {product.name}</h2>
+            <p>distance: {product.location}</p>
+            </section>
     ))
-
-    }
     return (
-        <div className='king.product'>
+        <div className={king.prod}>
         {productCat}
         </div>
     );
